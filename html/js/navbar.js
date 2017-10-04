eaed36416d869f8f41c769cadd61b2250ceec786
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-10-04 14:51:06 +0200

    Improved sidebar contents
    css animations for hamburger icon, infosupport logo and navigation buttons

--- html/js/navbar.js
+++ html/js/navbar.js
@@ -1,6 +1,19 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "250px";
+    // document.getElementById("hamburgerdiv").style.marginLeft = "250px";
+    document.getElementById("nav-icon3").setAttribute( "onclick", "closeNav()");
 }
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
+    // document.getElementById("hamburgerdiv").style.marginLeft = "0px";   
+    document.getElementById("nav-icon3").setAttribute( "onclick", "openNav()");
+    
+    
 }
+
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+        $(this).toggleClass('open');
+        $("#hamburgerdiv").toggleClass("open");
+	});
+});
