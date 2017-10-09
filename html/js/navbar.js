c664cf5a36bc8e583ae45d9fa9b004ab1b786568
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2017-10-09 10:39:26 +0200

    Updated navbar
    Navbar now slides into the screen instead of overlapping the content

--- html/js/navbar.js
+++ html/js/navbar.js
@@ -2,11 +2,13 @@ function openNav() {
     document.getElementById("sidebar").style.width = "250px";
     // document.getElementById("hamburgerdiv").style.marginLeft = "250px";
     document.getElementById("nav-icon3").setAttribute( "onclick", "closeNav()");
+    document.getElementById("main").style.marginLeft = "250px";
 }
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     // document.getElementById("hamburgerdiv").style.marginLeft = "0px";   
     document.getElementById("nav-icon3").setAttribute( "onclick", "openNav()");
+    document.getElementById("main").style.marginLeft = "0px";
     
     
 }
