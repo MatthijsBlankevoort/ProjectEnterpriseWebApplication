814122640b715705850d7dd4547ef1860d5e65a9
Thom <thomscholtens@gmail.com>
2017-11-28 23:59:45 +0100

    niet meer lelijke login

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -12,9 +12,61 @@
     <div id="sidebar" class="sidenav">
         <img src="images/logo-white.png" width="75%" height="" alt="">
         <hr>
-        <a href="/">Dashboard</a>        
+        <a href="/">Dashboard</a>
         <a href="/submitpost">New Post</a>
         <a href="/profile/create">Create Profile</a>
-        <a href="#">Log out</a>
-    </div>
+        @guest
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
+        @else
+            <!-- wil nog maken dat als je op je naam klikt in de navbar dat je eigen post ziet -->
+            <a href="/">{{ Auth::user()->name }}</a>
+
+
+
+            <a href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                            Logout
+            </a>
+
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+          </form>
+
+
+        @endguest
+      </div>
+
+        <!-- <ul class="nav navbar-nav navbar-right"> -->
+            <!-- Authentication Links -->
+            <!-- @guest
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            @endguest
+        </ul>
+
+
+    </div> -->
 </nav>
