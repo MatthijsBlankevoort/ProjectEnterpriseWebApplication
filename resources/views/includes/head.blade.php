dc1582699c5dd347274db13348d01751a9be4769
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2018-01-08 20:32:25 +0100

    Cleaned up code

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,16 +8,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha256-j+P6EZJVrbXgwSR5Mx+eCS6FvP9Wq27MBRC/ogVriY0=" crossorigin="anonymous" />
 <link rel="stylesheet" href="/assets/css/style.css">
 
- {{--  <script>
-         function getLikes(){
-            $.ajax({
-               type:'GET',
-               url:'/getlikes',
-               data: '',
-               success:function(data){
-                   
-               }
-            });
-         }
-</script>  --}}
 
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,108 +1,60 @@
 @if (session('message'))
 
-<div class="alert alert-info"><i class="fa fa-info-circle" aria-hidden="true"></i> {{ session('message') }}</div>
+<div class="alert alert-info">
+	<i class="fa fa-info-circle" aria-hidden="true"></i> {{ session('message') }}</div>
 
 @endif
 
-    <div id="hamburgerdiv">
-        <div id="nav-icon" onclick="openNav()">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+<div id="hamburgerdiv">
+	<div id="nav-icon" onclick="openNav()">
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
 
 
-        </div>
-    </div>
-    <div id="sidebar" class="sidenav">
-        @guest
-            <hr>
-            <a href="/">Dashboard</a>
-            <a href="{{ route('login') }}">Login</a>
-            <a href="{{ route('register') }}">Register</a>
-        @else
-            <a href="/profile">{{ Auth::user()->name }}</a>
-            <hr>
-            <a href="/">Dashboard</a>
-            <a href="/submitpost">New Post</a>
-            <a href="/home">Post Overview</a>
+	</div>
+</div>
+<div id="sidebar" class="sidenav">
+	@guest
+	<hr>
+	<a href="/">Dashboard</a>
+	<a href="{{ route('login') }}">Login</a>
+	<a href="{{ route('register') }}">Register</a>
+	@else
+	<a href="/profile">{{ Auth::user()->name }}</a>
+	<hr>
+	<a href="/">Dashboard</a>
+	<a href="/submitpost">New Post</a>
+	<a href="/home">Post Overview</a>
 
 
-            <hr>
-            <a href="{{ route('logout') }}"
-                  onclick="event.preventDefault();  
+	<hr>
+	<a href="{{ route('logout') }}" onclick="event.preventDefault();  
                         document.getElementById('logout-form').submit();">
-                            Logout
-            </a>
+		Logout
+	</a>
 
 
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              {{ csrf_field() }}
-          </form>
+	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+		{{ csrf_field() }}
+	</form>
 
-        @endguest
+	@endguest
 
-      </div>
-
-  
-    @guest
-
-    @else
-        @if(Request::is('/') || Request::is('sorted'))
-           <div class="dropdown">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sort By
-            </a>
-
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a href="{{ url('/sorted') }}" class="btn btn-block btn-lg">Likes</a>
-                <a href="{{ url('/') }}" class="btn btn-block btn-lg">Most Recent</a>
-                {{--<a class="dropdown-item" href="#">Something else here</a>--}}
-            </div>
-        </div>
-        @endif
-    @endguest
-
-    
+</div>
 
 
-      <!-- <div class="form-group" style="width: 10%;">
-        <label for="SortBy">Sort By</label>
-        <select name="Sort" class="form-control" id="Sort">
+@guest @else @if(Request::is('/') || Request::is('sorted'))
+<div class="dropdown">
+	<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+	 aria-expanded="false">
+		Sort By
+	</a>
 
-            <option value="{{action('PostsController@sortBy')}}">Most Liked</option>
-
-
-        </select>
-      </div> -->
-
-        <!-- <ul class="nav navbar-nav navbar-right"> -->
-            <!-- Authentication Links -->
-            <!-- @guest
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
-            @else
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                Logout
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    </ul>
-                </li>
-            @endguest
-        </ul>
-
-
-    </div> -->
+	<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+		<a href="{{ url('/sorted') }}" class="btn btn-block btn-lg">Likes</a>
+		<a href="{{ url('/') }}" class="btn btn-block btn-lg">Most Recent</a>
+	</div>
+</div>
+@endif @endguest
